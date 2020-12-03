e6eb49910f6b863f1d2c00a023dde45bf19ab960
Ike Maldonado <maldonadoike@gmail.com>
2020-12-02 21:21:59 -0800

    worked on controllers

--- src/controller/AddPartController.java
+++ src/controller/AddPartController.java
@@ -1,4 +1,35 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class AddPartController {
+    public void onActionNamePartTextField(ActionEvent actionEvent) {
+    }
+
+    public void onActionInvPartTextField(ActionEvent actionEvent) {
+    }
+
+    public void onActionPricePartTextField(ActionEvent actionEvent) {
+    }
+
+    public void onActionMaxPartTextField(ActionEvent actionEvent) {
+    }
+
+    public void onActionMachineIdPartTextField(ActionEvent actionEvent) {
+    }
+
+    public void onActionMinPartTextField(ActionEvent actionEvent) {
+    }
+
+    public void onActionSavePart(ActionEvent actionEvent) {
+    }
+
+    public void onActionCancelPart(ActionEvent actionEvent) {
+    }
+
+    public void onActionInhousePartRadio(ActionEvent actionEvent) {
+    }
+
+    public void onActionOursourcedPartRadio(ActionEvent actionEvent) {
+    }
 }
--- src/controller/AddProductController.java
+++ src/controller/AddProductController.java
@@ -1,4 +1,35 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class AddProductController {
+    public void onActionNameAddProduct(ActionEvent actionEvent) {
+    }
+
+    public void onActionInvAddProduct(ActionEvent actionEvent) {
+    }
+
+    public void onActionPriceAddProduct(ActionEvent actionEvent) {
+    }
+
+    public void onActionMaxAddProduct(ActionEvent actionEvent) {
+    }
+
+    public void onActionMinAddProduct(ActionEvent actionEvent) {
+    }
+
+    public void onActionSearchAddProduct(ActionEvent actionEvent) {
+    }
+
+    public void onActionAddAddProductButton(ActionEvent actionEvent) {
+    }
+
+    public void onActionRemoveAssociatedAddProduct(ActionEvent actionEvent) {
+    }
+
+    public void onActionSaveAddProduct(ActionEvent actionEvent) {
+    }
+
+    public void onActionCancelAddProduct(ActionEvent actionEvent) {
+    }
 }
