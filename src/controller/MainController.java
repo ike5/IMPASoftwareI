cd3ca0ee0fee16c6691abaf272e0b2c0e94808ff
Ike Maldonado <maldonadoike@gmail.com>
2020-12-02 19:21:58 -0800

    more views created and worked on

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,4 +1,98 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
 public class MainController {
+
+    @FXML
+    private TextField searchPartTextField;
+
+    @FXML
+    private TableColumn<?, ?> partIdColumn;
+
+    @FXML
+    private TableColumn<?, ?> partNameColumn;
+
+    @FXML
+    private TableColumn<?, ?> partInventoryLevelColumn;
+
+    @FXML
+    private TableColumn<?, ?> partPricePerUnitColumn;
+
+    @FXML
+    private Button addPartButton;
+
+    @FXML
+    private Button modifyPartButton;
+
+    @FXML
+    private Button deletePartButton;
+
+    @FXML
+    private TextField searchProductTextField;
+
+    @FXML
+    private TableColumn<?, ?> productIdColumn;
+
+    @FXML
+    private TableColumn<?, ?> productNameColumn;
+
+    @FXML
+    private TableColumn<?, ?> productInventoryLevelColumn;
+
+    @FXML
+    private TableColumn<?, ?> productPricePerUnitColumn;
+
+    @FXML
+    private Button addProductButton;
+
+    @FXML
+    private Button modifyProductButton;
+
+    @FXML
+    private Button deleteProductButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    void onActionAddPart(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionAddProduct(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionDeletePart(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionDeleteProduct(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionExit(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    void onActionModifyPart(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionModifyProduct(ActionEvent event) {
+
+    }
+
 }
+
